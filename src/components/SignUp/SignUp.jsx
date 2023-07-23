1aedaf0ff4b4bb61e4666545127f366e1e0e1793
Sumon Chandra Das <sumon.chonro.das@gmail.com>
2023-07-23 11:25:44 +0600

    adding done

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-import './Login.css'
+import "./SignUp.css";
+import { Link } from 'react-router-dom';
 
-const Login = () => {
+const SignUp = () => {
     return (
-    <div className='form-container'>
-            <h1 className="form-title">Login</h1>
+        <div className='form-container'>
+            <h1 className="form-title">Sign Up</h1>
             <form>
                 <div className="form-control">
                     <label htmlFor="email">Email</label>
@@ -15,11 +15,15 @@ const Login = () => {
                     <label htmlFor="password">Password</label>
                     <input type="password" name="password" id="" required/>
                 </div>
+                <div className="form-control">
+                    <label htmlFor="password">Confirm password</label>
+                    <input type="password" name="confirm" id="" required/>
+                </div>
                 <input type="submit" value="Login" className='btn-submit' />
             </form>
-            <p><small>new to ema-jon? <Link to='/signup'><span>Creat new account</span></Link></small></p>
+                <p><small>Already have an account? <Link to='/login'><span>Login</span></Link></small></p>
         </div>
     );
 };
 
-export default Login;
+export default SignUp;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBrDHesn-n_bmVo_e3HoMhWzSli-1Y8jQ8",
+  authDomain: "ema-john-simple-with-rou-e3f39.firebaseapp.com",
+  projectId: "ema-john-simple-with-rou-e3f39",
+  storageBucket: "ema-john-simple-with-rou-e3f39.appspot.com",
+  messagingSenderId: "357147402891",
+  appId: "1:357147402891:web:e1c0facffd5bdc7543ea8e"
+};
+
+// Initialize Firebase
+ const app = initializeApp(firebaseConfig);
+
+ export default app;
